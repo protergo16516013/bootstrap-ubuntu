cd0804c4260653d42ffa48953060da80b2b91d66
protergo16516013 <128043027+protergo16516013@users.noreply.github.com>
2025-03-19 11:23:07 +0700

    add: added everything needed for boot straping new install

---
+++ ubuntu-bootstrap.sh
@@ -0,0 +1,44 @@
+### docker installation
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+
+### dependency
+apt install -y \
+    git curl \
+    wget vim
+
+
+### misc dependency installation
+# golang
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt update
+sudo apt install -y golang-go
+# python
+sudo apt install -y python3 python3-pip
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+### qol imporvement
+# neovim
+sudo apt install -y neovim
+# lazydock
+go install github.com/jesseduffield/lazydocker@latest
+
+source ~/.bashrc
